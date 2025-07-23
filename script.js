9bf654c4114aef7e46686589443045f2c8ebee7d
GIBUBA2 <101049937+GIBUBA@users.noreply.github.com>
2025-07-23 13:58:04 -0300

    Atualização index, style, script.js

--- script.js
+++ script.js
@@ -6,17 +6,33 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
+        enunciado: "Você conhece a IA?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "Sim",
+            "Não"
         ]
     },
     {
-        enunciado: "Pergunta 2",
+        enunciado: "A opção de fazer pesquisas na IA deve ser por:",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "Pergunta simples",
+            "Prompt"
         ]
-    }
+    },
+    {
+        enunciado: "Para solicitar uma imagem na IA, qual seria a melhor pergunta!",
+        alternativas: [
+            "Quero uma imagem de um cachorro",
+            "Imagem de um cachorro, com coleira verde, rabo pequeno, da raça Buldogue-campeiro"
+        ]
+    },
 ]; 
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+mostraPergunta();
